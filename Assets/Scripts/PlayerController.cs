7033587d92baea86892afb0d80c9f609320325ed
Tolga Caner <tolga@opera.com>
2016-08-14 19:29:57 +0300

    camera movement (3rd person)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,14 +3,17 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
+	public float speed;
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-	
+		rb = GetComponent<Rigidbody> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 
 
@@ -18,6 +21,8 @@ public class PlayerController : MonoBehaviour {
 		float moveHorizontal = Input.GetAxis ("Horizontal");
 		float moveVertical = Input.GetAxis ("Vertical");
 
-		Rigidbody
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
 	}
 }
